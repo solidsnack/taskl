fe759e278f8f21dce6911d4130d3acb39f3d3b9b
Jason Dusek <jason.dusek@gmail.com>
2010-10-08 04:14:02 -0700

    Complete; probably can even be used to set up some stuff.

--- model.bash
+++ model.bash
@@ -9,18 +9,33 @@ set -o errtrace
 set -o noglob
 set +o histexpand
 
+ERR_UNKNOWN_TASK=16
+ERR_BAD_ARGS=8
 
-# Peform the check for the given operation. Only atomic operations have
-# checks.
-function check {
-  case "${1}" in
-    $'fs/own:/usr/foo\nbar')
+operation='Task\L command never set; what gives?'
+depth=0
+task=''
+waiting=false
+
+
+function notify {
+  printf "%${depth}s${1}\\0%s\\0\\n" '' "$2"
+  $waiting && receive
+}
+
+
+# Code for checks, messages and actions. Only atomic actions are found in
+# these case statements.
+
+function checks {
+  case "$1" in
+    $'fs/ownership:/usr/foo\nbar')
       echo "Would be test code."
       ;;
     $'fs/mode:/usr/foo\nbar')
       echo "Would be test code."
       ;;
-    $'fs/obj:/usr/foo\nbar')
+    $'fs/layout:/usr/foo\nbar')
       echo "Would be test code."
       ;;
     $'pw/user:mo')
@@ -28,101 +43,168 @@ function check {
       ;;
     *)
       echo 'Mysterious call to unknown task.' 1>&2
-      exit 16
+      exit $ERR_UNKNOWN_TASK
       ;;
   esac
 }
 
+function messages {
+  case "$1" in
+    $'fs/ownership:/usr/foo\nbar')
+      notify ' ** ' "Marking change to ownership."
+      ;;
+    $'fs/mode:/usr/foo\nbar')
+      notify ' ** ' "Marking change to mode."
+      ;;
+    $'fs/layout:/usr/foo\nbar')
+      notify ' ** ' "Marking change for file contents."
+      ;;
+    $'pw/user:mo')
+      notify ' ** ' "Marking change for user's passwd DB entry."
+      ;;
+    *)
+      echo 'Mysterious call to unknown task.' 1>&2
+      exit $ERR_UNKNOWN_TASK
+      ;;
+  esac
+}
+
+function actions {
+  case "$1" in
+    $'fs/ownership:/usr/foo\nbar')
+      echo "Run code here."
+      ;;
+    $'fs/mode:/usr/foo\nbar')
+      echo "Run code here."
+      ;;
+    $'fs/layout:/usr/foo\nbar')
+      echo "Run code here."
+      ;;
+    $'pw/user:mo')
+      echo "Run code here."
+      ;;
+    *)
+      echo 'Mysterious call to unknown task.' 1>&2
+      exit $ERR_UNKNOWN_TASK
+      ;;
+  esac
+}
+
+
+# The operations. All of them rely on `branch' to route them through the
+# package hierarchy.
+
+function check {
+  notify ' >> ' "$task"
+  let depth++
+  branch "$task"
+  let depth--
+}
+
+function install {
+  notify ' >> ' "$task"
+  let depth++
+  branch "$task"
+  let depth--
+  notify ' << ' "$task"
+}
+
+function list {
+  notify ' ' "$task"
+  branch "$task"
+}
+
 function has {
-  if [[ ${#1} -eq 0 ]]
+  if branch "$task"
   then
     echo true
     exit 0
   else
     echo false
-    exit 8
+    exit $ERR_UNKNOWN_TASK
   fi
 }
 
-function tasks {
-  local task="$1"
-  local operation="$2"
-  local depth="$3"
-  if ! [[ ${#task} -eq 0 ]]
-  then
-    $no_inmessage || printf "%${depth}s${in_symbol}%s\0\n" '' "${task}"
-    let depth++
-  fi
-  case "${task}" in
-    '')
-      $operation '' $depth
-      tasks $'fs:/usr/foo\nbar'
-      tasks $'user:mo'
+
+# Leaf and branch, together, allow packages to be resolved to their atomic
+# actions.
+
+function leaf {
+  case "$operation" in
+    has|list)
+      return 0
       ;;
-    $'fs:/usr/foo\nbar')
-      $operation "$task" $depth
-      tasks $'fs/obj:/usr/foo\nbar'
-      tasks $'fs/own:/usr/foo\nbar'
-      tasks $'fs/mode:/usr/foo\nbar'
-      ;;
-    *)
-    $'fs/own:/usr/foo\nbar')
-      $operation "$task" $depth
-      ;;
-    $'fs/mode:/usr/foo\nbar')
-      $operation "$task" $depth
-      ;;
-    $'fs/obj:/usr/foo\nbar')
-      $operation "$task" $depth
-      ;;
-    $'pw/user:mo')
-      $operation "$task" $depth
-      ;;
-    *)
-      $operation '' $depth
-      echo 'Mysterious call to unknown task.' 1>&2
-      exit 16
+    check|install)
+      if already_handled "$task"
+      then
+        ${marked["$task"]} && notify ' ** ' 'Previously marked for change.'
+      else
+        if ! checks "$task"
+        then
+          messages "$task"
+          [ "$operation" = 'install' ] && actions "$task"
+          marked["$task"]=true
+        else
+          marked["$task"]=false
+        fi
+      fi
       ;;
   esac
-  if ! [[ ${#task} -eq 0 ]]
-  then
-    $no_outmessage || printf "%${depth}s << %s\0\n" '' "${task}"
-    let depth--
-  fi
+}
+
+function branch {
+  case "$task" in
+    $'fs:/usr/foo\nbar')
+      "$operation" $'fs/layout:/usr/foo\nbar'
+      "$operation" $'fs/ownership:/usr/foo\nbar'
+      "$operation" $'fs/mode:/usr/foo\nbar'
+      ;;
+    $'fs/layout:/usr/foo\nbar')
+      leaf
+      ;;
+    $'fs/ownership:/usr/foo\nbar')
+      leaf
+      ;;
+    $'fs/mode:/usr/foo\nbar')
+      leaf
+      ;;
+    $'pw/user:mo')
+      leaf
+      ;;
+    *)
+      return $ERR_UNKNOWN_TASK
+      ;;
+  esac
 }
 
 
-mode=''
-in_symbol=' >> '
-no_outmessage=false
-no_inmessage=false
-depth=0
-case "${1}" in
-  install)
-    mode=$1
-    ;;
-  check)
-    mode=$1
-    ;;
-  list)
-    mode=$1
-    no_outmessage=true
-    in_symbol=''
-    ;;
-  has)
-    mode=$1
-    no_outmessage=true
-    no_inmessage=true
+function dispatch {
+  case "$1" in
+    install|check|list|has)
+      operation=$1
+      ;;
+    *)
+      # Check if it's a number; that's how we'll communicate depth.
+      if its_a_number
+      then
+        depth=$1
+        shift
+        dispatch "$@"
+      else
+        echo "Argument error." 1>&2
+        exit $ERR_BAD_ARGS
+      fi
+      ;;
+  esac
+}
+
+case "$2" in
+  '')
+    "$operation" $'fs:/usr/foo\nbar'
+    "$operation" $'pw/user:mo'
     ;;
   *)
-    # Check if it's a number; that's how we'll communicate depth.
-    depth=${1}
-    shift
-    ;;
-esac
-
-case "${2}" in
-  '')
+    "$operation" "$2"
     ;;
 esac
 
