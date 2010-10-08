af20725ec3d04623c18bc299e122dba03db5cea6
Jason Dusek <jason.dusek@gmail.com>
2010-10-07 19:54:58 -0700

    Rewrite.

--- model.bash
+++ model.bash
@@ -9,26 +9,6 @@ set -o errtrace
 set -o noglob
 set +o histexpand
 
-declare -A schedule
-notified[$'fs:/usr/foo\nbar']=false
-notified[$'fs/obj:/usr/foo\nbar']=false
-notified[$'fs/own:/usr/foo\nbar']=false
-notified[$'fs/mode:/usr/foo\nbar']=false
-notified[$'user:mo']=false
-
-declare -A notified
-notified[$'fs:/usr/foo\nbar']=false
-notified[$'fs/obj:/usr/foo\nbar']=false
-notified[$'fs/own:/usr/foo\nbar']=false
-notified[$'fs/mode:/usr/foo\nbar']=false
-notified[$'user:mo']=false
-
-declare -A entered
-entered[$'fs:/usr/foo\nbar']=false
-entered[$'fs/obj:/usr/foo\nbar']=false
-entered[$'fs/own:/usr/foo\nbar']=false
-entered[$'fs/mode:/usr/foo\nbar']=false
-entered[$'user:mo']=false
 
 # Peform the check for the given operation. Only atomic operations have
 # checks.
@@ -46,42 +26,103 @@ function check {
     $'pw/user:mo')
       echo "Would be test code."
       ;;
+    *)
+      echo 'Mysterious call to unknown task.' 1>&2
+      exit 16
+      ;;
   esac
 }
 
-# Place items in the schedule as needed. Only composite tasks get their own
-# case statements; atomic tasks default to check and schedule.
-declare -a stack=()
-function mark {
-  local task="${1}"
-  stack[${#tasks_stack[@]}]="$task"
-  case "$task" in
+function has {
+  if [[ ${#1} -eq 0 ]]
+  then
+    echo true
+    exit 0
+  else
+    echo false
+    exit 8
+  fi
+}
+
+function tasks {
+  local task="$1"
+  local operation="$2"
+  local depth="$3"
+  if ! [[ ${#task} -eq 0 ]]
+  then
+    $no_inmessage || printf "%${depth}s${in_symbol}%s\0\n" '' "${task}"
+    let depth++
+  fi
+  case "${task}" in
+    '')
+      $operation '' $depth
+      tasks $'fs:/usr/foo\nbar'
+      tasks $'user:mo'
+      ;;
     $'fs:/usr/foo\nbar')
-      mark $'fs/obj:/usr/foo\nbar'
-      mark $'fs/own:/usr/foo\nbar'
-      mark $'fs/mode:/usr/foo\nbar'
+      $operation "$task" $depth
+      tasks $'fs/obj:/usr/foo\nbar'
+      tasks $'fs/own:/usr/foo\nbar'
+      tasks $'fs/mode:/usr/foo\nbar'
       ;;
     *)
-      if check ${task}
-      then
-        for i in ${!stack[@]}
-        do
-          local this_task=stack[$i]
-          if ! schedule["${this_task}"]
-          then
-            for x in `seq 1 $i`
-            do
-              echo -n ' '
-            done
-            echo -n '+ '
-            echo "${i}" 
-            schedule[$i]=true
-          fi
-        done
-      fi
+    $'fs/own:/usr/foo\nbar')
+      $operation "$task" $depth
+      ;;
+    $'fs/mode:/usr/foo\nbar')
+      $operation "$task" $depth
+      ;;
+    $'fs/obj:/usr/foo\nbar')
+      $operation "$task" $depth
+      ;;
+    $'pw/user:mo')
+      $operation "$task" $depth
+      ;;
+    *)
+      $operation '' $depth
+      echo 'Mysterious call to unknown task.' 1>&2
+      exit 16
       ;;
   esac
-  unset stack[${#tasks_stack[@]}-1]
+  if ! [[ ${#task} -eq 0 ]]
+  then
+    $no_outmessage || printf "%${depth}s << %s\0\n" '' "${task}"
+    let depth--
+  fi
 }
 
+
+mode=''
+in_symbol=' >> '
+no_outmessage=false
+no_inmessage=false
+depth=0
+case "${1}" in
+  install)
+    mode=$1
+    ;;
+  check)
+    mode=$1
+    ;;
+  list)
+    mode=$1
+    no_outmessage=true
+    in_symbol=''
+    ;;
+  has)
+    mode=$1
+    no_outmessage=true
+    no_inmessage=true
+    ;;
+  *)
+    # Check if it's a number; that's how we'll communicate depth.
+    depth=${1}
+    shift
+    ;;
+esac
+
+case "${2}" in
+  '')
+    ;;
+esac
 
