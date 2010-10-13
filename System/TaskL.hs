09944627084ba440132cca154d4203280275c367
Jason Dusek <jason.dusek@gmail.com>
2010-10-12 23:08:25 -0700

    Moving to back to resources. Resources or idempotent operations -- what's a better primitive?
    If you go with idempotent operations, it's hard to build resources out of them
    unless you can extend the checks in an operation. Say for example we have `rm
    <path>'. It's run if the <path> is there. Later, we want to have some
    fanciness to `rm'; like, only `rm' if there is a symlink link at <path> that
    points to `/dev/null'. We approach this as a "filesystem node" resource that
    implies a lot of idempotent operations and checks. Now how do we add extra
    checks to the `rm' so that it does not simply nuke the path if it is there?

---
+++ System/TaskL.hs
@@ -0,0 +1,64 @@
+
+{-# LANGUAGE TypeFamilies
+           , EmptyDataDecls
+           , OverloadedStrings
+           , StandaloneDeriving
+ #-}
+
+module System.TaskL where
+
+import Data.String
+import Control.Applicative
+import Control.Monad.Identity
+
+import Data.Text
+
+
+data Task t where
+  Setup         ::  (SystemObject t) => Label t -> Spec t Identity -> Task t
+  Teardown      ::  (Monad m, SystemObject t) => Label t -> Spec t m -> Task t
+
+
+data Tasks where
+  Tasks         ::  forall t. (SystemObject t) => [Task t] -> Tasks
+
+
+class SystemObject t where
+  type Label t              ::  *
+  type Spec t               ::  (* -> *) -> *
+  check         ::  (Monad m) => Label t -> Spec t m -> ShellScript
+  rm                        ::  Label t -> ShellScript
+  make                      ::  Label t -> Spec t Identity -> ShellScript
+
+
+data FilesystemNode
+instance SystemObject FilesystemNode where
+  type Label FilesystemNode  =  Path
+  type Spec FilesystemNode   =  NodeAttrs
+
+data UserEntry
+instance SystemObject UserEntry where
+  type Label UserEntry       =  User
+  type Spec UserEntry        =  UserAttrs
+
+data GroupEntry
+instance SystemObject GroupEntry where
+  type Label GroupEntry      =  Group
+  type Spec GroupEntry       =  GroupAttrs
+
+
+
+
+data Path
+data Own
+data Mode
+data NodeAttrs              ::  (* -> *) -> *
+
+data User
+data UserAttrs              ::  (* -> *) -> *
+data Group
+data GroupAttrs             ::  (* -> *) -> *
+
+newtype ShellScript          =  ShellScript Text
+deriving instance IsString ShellScript
+
