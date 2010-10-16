1cfedd147e3f42c500b9852c35b1c3a9f90e2003
Jason Dusek <jason.dusek@gmail.com>
2010-10-15 23:21:52 -0700

    Notes on DAG/Schedule layers.

--- System/TaskL.hs
+++ System/TaskL.hs
@@ -13,54 +13,39 @@ import Control.Monad.Identity
 
 import Data.Text
 
-import System.TaskL.IdemShell
+import qualified System.TaskL.IdemShell as IdemShell
 
 
-data Task label              =  Atomic label [Test] Command
-                             |  Aggregate label [Test] [Task label]
+{-| How this is supposed to work:
+
+* Client generates TaskDAG.
+
+* Scheduler in TaskL creates schedule. Backends compile schedule.
+
+* The tasks are defined in terms of IdemShell, the idempotent shell, which
+  is also exposed to clients. What does the idempotent have to say to clients?
+  A command has a test. If the test succeeds, the command is not executed.
+  If the command is executed, however, the test definitely will succeed the
+  next time. You can add more tests in a way that causes the command not to
+  run in more scenarios (hence we have @[IdemShell.Test]@ terms in all the
+  'TaskL' terms); this does not wreck the idempotence.
+
+* This adding more tests business should perhaps be shoved down in to
+  IdemShell; but then how do we add tests to aggregates? It puzzles me.
+
+ -}
 
 
-
-data Tasks where
-  Tasks         ::  forall t. (SystemObject t) => [Task t] -> Tasks
-
-
-class SystemObject t where
-  type Label t              ::  *
-  type Spec t               ::  (* -> *) -> *
-  check         ::  (Monad m) => Label t -> Spec t m -> ShellScript
-  rm                        ::  Label t -> ShellScript
-  make                      ::  Label t -> Spec t Identity -> ShellScript
+data TaskL label             =  Atomic label [IdemShell.Test] IdemShell.Command
+                             |  Aggregate label [IdemShell.Test] [Task label]
 
 
-data FilesystemNode
-instance SystemObject FilesystemNode where
-  type Label FilesystemNode  =  Path
-  type Spec FilesystemNode   =  NodeAttrs
-
-data UserEntry
-instance SystemObject UserEntry where
-  type Label UserEntry       =  User
-  type Spec UserEntry        =  UserAttrs
-
-data GroupEntry
-instance SystemObject GroupEntry where
-  type Label GroupEntry      =  Group
-  type Spec GroupEntry       =  GroupAttrs
+data Schedule label          =  Enter label
+                             |  Leave label
+                             |  Check label [IdemShell.Test]
+                             |  Perform label [IdemShell.Command]
 
 
 
 
-data Path
-data Own
-data Mode
-data NodeAttrs              ::  (* -> *) -> *
-
-data User
-data UserAttrs              ::  (* -> *) -> *
-data Group
-data GroupAttrs             ::  (* -> *) -> *
-
-newtype ShellScript          =  ShellScript Text
-deriving instance IsString ShellScript
 
