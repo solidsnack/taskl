f3271a0b29b46c7c073f76081113e795bc6a327c
Jason Dusek <jason.dusek@gmail.com>
2010-10-22 22:07:59 -0700

    Fleshed out scheduling a bit. Conflict detection and overlap resolution need to be implemented in IdemShell.

--- System/TaskL.hs
+++ System/TaskL.hs
@@ -5,46 +5,91 @@
            , StandaloneDeriving
  #-}
 
+{-| The Task\\L system provides a language for configuring Linux machines (and
+    it probably works on other UNIX-alikes, provided the GNU core-utils are
+    installed).
+
+    Tasks in Task\\L are idempotent by design; one may build up packages of
+    idempotent actions from a library of primitive commands that each resemble
+    a classic UNIX command (see the @IdemShell@ docs to find out more about
+    these commands).
+
+    The input to the Task\\L system is a 'Tree' of 'Task' items, specifying
+    operations and their dependencies. Multiple declarations of the same task
+    are merged and contradictions checked for before the whole structure is
+    flattened into a linear schedule in which each attribute -- a file's
+    permissions, a group's membership list -- is touched but one time.
+
+    The schedule is compiled to a Bash script which may then be executed to
+    instantiate the configuration. As it runs, the script gives notes which
+    package (or packages) it is operating on as well any subtasks; the script
+    may be run in an interactive mode that allows delaying, skipping or
+    aborting at any step.
+
+ -}
+
 module System.TaskL where
 
+import Data.Tree
 import Data.String
 import Control.Applicative
 import Control.Monad.Identity
 
 import Data.ByteString
+import Data.Number.Natural
 
 import qualified System.TaskL.IdemShell as IdemShell
 
 
-{-| The compiler input is this 'TaskGraph', a DAG of tasks. One may attach
-    tests and dependencies to every node.
+{-| A task may be either a command or a named package. Commands provide their
+    own labels. Every 'Task' may attach tests to restrict the circumstances
+    under which a command is run.
+ -}
+data Task                    =  Command IdemShell.Command [IdemShell.Test]
+                             |  Package ByteString [IdemShell.Test]
 
-    The 'TaskGraph' is reduced to a sequence of operations in the 'Op' type.
-    The schedule is such that each operation on a given system object -- for
-    example, changing permissions or adding a user -- is executed but one
-    time. Packages with overlapping operations are interleaved as necessary.
 
-    TaskL can merge operations that overlap in their effect but do not
-    conflict in order to follow the "only once" rule. For example, if one task
-    specifies the user permissions and another the group permissions, these
-    can be turned in to one operation that sets both. When conflicts between
-    similar operations can not be resolved, the compiler will signal an error.
+{-| The script backend consumes a schedule, a list of basic operations, which
+    is constructed from a tree of 'Task' nodes. The tree structure encodes
+    dependencies among tasks.
+
+    The tree is first translated to a tree in which every task is labelled
+    with its position in the tree, giving a notion of the desired ordering.
+    The tree is then collapsed to a list in which like tasks are merged and
+    paired with their collective dependents. At this stage, contradictions are
+    detected; unmergeable operations result in an error.
+
+    The commands are then scheduled according to these guidelines:
+
+   *  Dependencies must come before their dependents.
+
+   *  It would be nice if tasks that were earlier in the input came before
+      commands that came later in the input.
+
+   *  Commands with multiple dependents should be grouped so that each
+      dependent is being run for as short a time as is practical.
 
  -}
-data TaskGraph               =  TaskGraph Task [IdemShell.Test] [TaskGraph]
+schedule                    ::  Tree Task -> Either Error [Op]
+schedule                     =  undefined
 
-{-| A task may be either a command or a package. Commands provide their own
-    labels. 
- -}
-data Task                    =  Command IdemShell.Command
-                             |  Package ByteString
 
-{-| The operations every backend must support.
+{-| Labels every node with a sequence of indices into the tree, pairing the
+    sequence with the node's subtree.
  -}
-data Op                      =  Enter ByteString
-                             |  Leave ByteString
-                             |  Check ByteString [IdemShell.Test]
-                             |  Perform IdemShell.Command
+label                       ::  Tree Task -> Tree ([Natural], Tree Task)
+label                        =  undefined
+
+
+{-| A backend supports these operations.
+ -}
+data Op
+  = Enter ByteString                  -- ^ Notify that task is starting.
+  | Leave ByteString                  -- ^ Notify that task is complete.
+  | Check ByteString [IdemShell.Test] -- ^ Perform check for labelled task.
+  | Perform IdemShell.Command         -- ^ Execute command if necessary. 
+
+
 
 
 
