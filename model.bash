4f5a1bcbf25586e0f160f0e48b122a072b0e6730
Jason Dusek <jason.dusek@gmail.com>
2010-10-26 16:46:25 -0700

    More realistic/horrible code.

--- model.bash
+++ model.bash
@@ -12,192 +12,117 @@ set +o histexpand
 ERR_UNKNOWN_TASK=16
 ERR_BAD_ARGS=8
 
-operation='Task\L command never set; what gives?'
-depth=0
-task=''
-waiting=false
+
+##  State storage.
+
+check_state=false
+
+declare -A options=(
+[0]=false
+[interactive]=false
+)
+
+declare -A enabled=(
+[$'task:pg_conf']=false
+[$'task:my_conf']=false
+)
 
 
-function notify {
-  printf "%${depth}s${1}\\0%s\\0\\n" '' "$2"
-  $waiting && receive
-}
+##  Notification, interaction and formatting utilities.
 
-
-# Code for checks, messages and actions. Only atomic actions are found in
-# these case statements.
-
-function checks {
-  case "$1" in
-    $'fs/ownership:/usr/foo\nbar')
-      echo "Would be test code."
-      ;;
-    $'fs/mode:/usr/foo\nbar')
-      echo "Would be test code."
-      ;;
-    $'fs/layout:/usr/foo\nbar')
-      echo "Would be test code."
-      ;;
-    $'pw/user:mo')
-      echo "Would be test code."
-      ;;
-    *)
-      echo 'Mysterious call to unknown task.' 1>&2
-      exit $ERR_UNKNOWN_TASK
-      ;;
-  esac
-}
-
-function messages {
-  case "$1" in
-    $'fs/ownership:/usr/foo\nbar')
-      notify ' ** ' "Marking change to ownership."
-      ;;
-    $'fs/mode:/usr/foo\nbar')
-      notify ' ** ' "Marking change to mode."
-      ;;
-    $'fs/layout:/usr/foo\nbar')
-      notify ' ** ' "Marking change for file contents."
-      ;;
-    $'pw/user:mo')
-      notify ' ** ' "Marking change for user's passwd DB entry."
-      ;;
-    *)
-      echo 'Mysterious call to unknown task.' 1>&2
-      exit $ERR_UNKNOWN_TASK
-      ;;
-  esac
-}
-
-function actions {
-  case "$1" in
-    $'fs/ownership:/usr/foo\nbar')
-      echo "Run code here."
-      ;;
-    $'fs/mode:/usr/foo\nbar')
-      echo "Run code here."
-      ;;
-    $'fs/layout:/usr/foo\nbar')
-      echo "Run code here."
-      ;;
-    $'pw/user:mo')
-      echo "Run code here."
-      ;;
-    *)
-      echo 'Mysterious call to unknown task.' 1>&2
-      exit $ERR_UNKNOWN_TASK
-      ;;
-  esac
-}
-
-
-# The operations. All of them rely on `branch' to route them through the
-# package hierarchy.
-
-function check {
-  notify ' >> ' "$task"
-  let depth++
-  branch "$task"
-  let depth--
-}
-
-function install {
-  notify ' >> ' "$task"
-  let depth++
-  branch "$task"
-  let depth--
-  notify ' << ' "$task"
-}
-
-function list {
-  notify ' ' "$task"
-  branch "$task"
-}
-
-function has {
-  if branch "$task"
+function message {
+  if ${options[0]}
   then
-    echo true
-    exit 0
+    printf " %s %s\\0" "$2" "$3"
   else
-    echo false
-    exit $ERR_UNKNOWN_TASK
+    printf " %s %s\\n" "$2" "$3"
   fi
 }
 
-
-# Leaf and branch, together, allow packages to be resolved to their atomic
-# actions.
-
-function leaf {
-  case "$operation" in
-    has|list)
-      return 0
-      ;;
-    check|install)
-      if ! checks "$task"
-      then
-        messages "$task"
-        [ "$operation" = 'install' ] && actions "$task"
-      else
-      fi
-      ;;
-  esac
+function interact {
+  if ${options[interactive]}
+  then
+    read response
+    case "$response" in
+      OK)           info_notify  "Read \`OK', proceeding." ;;
+      ABORT)        abort_notify "Read \`ABORT', quitting." ;;
+      *)            error_notify "Read \`$response'; not understood." ;;
+    esac
+  fi
 }
 
-function branch {
-  case "$task" in
-    $'fs:/usr/foo\nbar')
-      "$operation" $'fs/layout:/usr/foo\nbar'
-      "$operation" $'fs/ownership:/usr/foo\nbar'
-      "$operation" $'fs/mode:/usr/foo\nbar'
-      ;;
-    $'fs/layout:/usr/foo\nbar')
-      leaf
-      ;;
-    $'fs/ownership:/usr/foo\nbar')
-      leaf
-      ;;
-    $'fs/mode:/usr/foo\nbar')
-      leaf
-      ;;
-    $'pw/user:mo')
-      leaf
-      ;;
-    *)
-      return $ERR_UNKNOWN_TASK
-      ;;
-  esac
-}
+function check_notify {  ${enabled["$1"]} && message G '**' "$1" && interact  }
+function exec_notify  {  ${enabled["$1"]} && message G '@@' "$1" && interact  }
+function enter_notify {  ${enabled["$1"]} && message P '>>' "$1" && interact  }
+function leave_notify {  ${enabled["$1"]} && message P '<<' "$1" && interact  }
+function info_notify  {                      message B '##' "$1"              }
+function abort_notify {                      message Y '~~' "$1" ; exit 0     }
+function error_notify {                      message R '!!' "$1" ; exit 1     }
 
 
-function dispatch {
-  case "$1" in
-    install|check|list|has)
-      operation=$1
-      ;;
-    *)
-      # Check if it's a number; that's how we'll communicate depth.
-      if its_a_number
-      then
-        depth=$1
-        shift
-        dispatch "$@"
-      else
-        echo "Argument error." 1>&2
-        exit $ERR_BAD_ARGS
-      fi
-      ;;
-  esac
-}
 
-case "$2" in
-  '')
-    "$operation" $'fs:/usr/foo\nbar'
-    "$operation" $'pw/user:mo'
-    ;;
-  *)
-    "$operation" "$2"
-    ;;
-esac
+
+##  Handle input.
+
+T=$1
+for task in "${enabled[@]}"
+do
+  enabled["$task"]=true
+done
+
+
+
+
+##  Installation code.
+
+enter_notify $'task:pg'
+check_state=true ## No check.
+{ ${enabled[$'task:pg']}
+  ${check_state}
+  enabled[$'fs:/etc/postgres.conf']=true
+  enabled[$'fs:/etc/hosts']=true }
+enter_notify $'fs:/etc/postgres.conf'
+check_notify $'fs:/etc/postgres.conf'
+{ ${enabled[$'fs:/etc/postgres.conf']}
+  check_state=false
+  ! diff -q ./etc/postgres.conf "$T"/etc/postgres.conf
+  check_state=true }
+exec_notify $'fs:/etc/postgres.conf'
+{ ${enabled[$'fs:/etc/postgres.conf']}
+  ${check_state}
+  exec_notify $'fs:/etc/postgres.conf'
+  cp -a ./etc/postgres.conf "$T"/etc/postgres.conf }
+leave_notify $'fs:/etc/postgres.conf'
+enter_notify $'task:postfix'
+check_state=true ## No check.
+{ ${enabled[$'task:postfix']}
+  ${check_state}
+  enabled[$'fs:/etc/postgres.conf']=true
+  enabled[$'fs:/etc/hosts']=true }
+enter_notify $'fs:/etc/hosts'
+check_notify $'fs:/etc/hosts'
+{ ${enabled[$'fs:/etc/hosts']}
+  check_state=false
+  ! diff -q ./etc/hosts "$T"/etc/hosts
+  check_state=true }
+exec_notify $'fs:/etc/hosts'
+{ ${enabled[$'fs:/etc/hosts']}
+  ${check_state}
+  exec_notify $'fs:/etc/hosts'
+  cp -a ./etc/hosts "$T"/etc/hosts }
+leave_notify $'fs:/etc/hosts'
+leave_notify $'task:pg'
+enter_notify $'fs:/etc/postfix.conf'
+check_notify $'fs:/etc/postfix.conf'
+{ ${enabled[$'fs:/etc/postfix.conf']}
+  check_state=false
+  ! diff -q ./etc/postfix.conf "$T"/etc/postfix.conf
+  check_state=true }
+exec_notify $'fs:/etc/postfix.conf'
+{ ${enabled[$'fs:/etc/postfix.conf']}
+  ${check_state}
+  exec_notify $'fs:/etc/postfix.conf'
+  cp -a ./etc/postfix.conf "$T"/etc/postfix.conf }
+leave_notify $'fs:/etc/postfix.conf'
+leave_notify $'task:postfix'
 
