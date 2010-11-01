0cf20e1c8cb199b367432ba9414330500a64702b
Jason Dusek <jason.dusek@gmail.com>
2010-10-31 21:44:07 -0700

    Conversion to ByteString.

---
+++ Data/ByteString/EncDec.hs
@@ -0,0 +1,31 @@
+
+{-# LANGUAGE StandaloneDeriving
+           , UndecidableInstances
+           , FlexibleInstances
+           , OverloadedStrings
+  #-}
+
+module Data.ByteString.EncDec where
+
+import Data.String
+import Control.Monad.Error
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as ByteString
+
+
+class EncDec t where
+  enc                       ::  t -> ByteString
+  dec                       ::  ByteString -> Either ByteString t
+instance EncDec ByteString where
+  enc                        =  id
+  dec                        =  return
+instance (Read n, Num n) => EncDec n where
+  enc                        =  ByteString.pack . show
+  dec t                      =  case (reads . ByteString.unpack) t of
+                                  [     ]     ->  throwError "Not a number."
+                                  (n,_):_     ->  return n
+
+instance Error ByteString where
+  strMsg                     =  ByteString.pack
+
