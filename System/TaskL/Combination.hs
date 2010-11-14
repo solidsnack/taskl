896f4d05d4a3fdc843b322f7ae6c21af5fb67630
Jason Dusek <jason.dusek@gmail.com>
2010-11-13 16:25:17 -0800

    Discovery/inspection for Combination.

--- System/TaskL/Combination.hs
+++ System/TaskL/Combination.hs
@@ -1,15 +1,27 @@
+{-# LANGUAGE StandaloneDeriving
+  #-}
 
 module System.TaskL.Combination where
+
 
 data Combination t           =  Contradictory t t
                              |  Combined t
                              |  Separate t t
- deriving (Eq, Ord, Show)
+deriving instance (Eq t) => Eq (Combination t)
+deriving instance (Ord t) => Ord (Combination t)
+deriving instance (Show t) => Show (Combination t)
+instance Functor Combination where
+  fmap f (Contradictory a b) =  Contradictory (f a) (f b)
+  fmap f (Combined a)        =  Combined (f a)
+  fmap f (Separate a b)      =  Separate (f a) (f b)
+
 
 class (Eq t) => Combine t where
   combine                   ::  t -> t -> Combination t
 
-contradictory               ::  Combination t -> Bool
-contradictory (Contradictory _ _) = True
-contradictory _              =  False
+
+draw                        ::  Combination String -> String
+draw (Contradictory a b)     =  "-----------Contradictory\n" ++ a ++ "\n" ++ b
+draw (Combined a)            =  "----------------Combined\n" ++ a
+draw (Separate a b)          =  "----------------Separate\n" ++ a ++ "\n" ++ b
 
