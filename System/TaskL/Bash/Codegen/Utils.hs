f8d35065decb8cef5ac9ff58e0d232e8a8ded13c
Jason Dusek <jason.dusek@gmail.com>
2010-12-05 00:30:12 -0800

    Remove useless utilities.

--- System/TaskL/Bash/Codegen/Utils.hs
+++ System/TaskL/Bash/Codegen/Utils.hs
@@ -1,26 +1,9 @@
-{-# LANGUAGE OverloadedStrings
-  #-}
-
 module System.TaskL.Bash.Codegen.Utils where
 
-import qualified Data.List as List
 import Data.ByteString
 import qualified Text.ShellEscape as Esc
 
 import Data.ByteString.EncDec
-import System.TaskL.IdemShell
-import System.TaskL.IdemShell.Path
-import System.TaskL.Bash.Program (cmd)
-import qualified System.TaskL.Bash.Program as Program
-
-
-testFS                      ::  ByteString -> Path -> Program.Term
-testFS t p                   =  cmd ["[", t, escEnc p, "]"]
-
-
-getent                      ::  GettableEnt -> Program.Term
-getent (User nick)           =  cmd ["getent", "passwd", escEnc nick]
-getent (Group nick)          =  cmd ["getent", "group", escEnc nick]
 
 
 escEnc                      ::  (EncDec t) => t -> ByteString
@@ -29,11 +12,5 @@ escEnc                       =  esc . enc
 
 esc                         ::  ByteString -> ByteString
 esc                          =  Esc.bytes . Esc.bash
-
-
-pipeline                    ::  [Program.Term] -> Program.Term
-pipeline []                  =  cmd ["true"]
-pipeline [term]              =  term
-pipeline (a:b:terms)         =  List.foldr Program.Pipe a (b:terms)
 
 
