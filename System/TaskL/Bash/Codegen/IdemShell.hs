81df3aa873d2d9a3cfa2703bf9b8c295ebe2e6f3
Jason Dusek <jason.dusek@gmail.com>
2010-12-05 18:48:26 -0800

    Numbers are numbers, no point in pretending...

--- System/TaskL/Bash/Codegen/IdemShell.hs
+++ System/TaskL/Bash/Codegen/IdemShell.hs
@@ -68,56 +68,58 @@ collapse test                =  test
 fullModeRE                  ::  Mode -> ByteString
 fullModeRE (Mode ur uw ux us gr gw gx gs or ow ox ot) =
   (concat . map (flippedModeRE ur uw ux us gr gw gx gs or ow ox ot))
-    [Idx0,Idx1,Idx2,Idx3,Idx4,Idx5,Idx6,Idx7,Idx8]
+    [Nine0,Nine1,Nine2,Nine3,Nine4,Nine5,Nine6,Nine7,Nine8]
  where
   flippedModeRE ur  uw  ux  us  gr  gw  gx  gs  or  ow  ox  ot  idx =
          modeRE idx ur  uw  ux  us  gr  gw  gx  gs  or  ow  ox  ot
 
 
-data ModeIdx = Idx0 | Idx1 | Idx2 | Idx3 | Idx4 | Idx5 | Idx6 | Idx7 | Idx8
+data Nine = Nine0 | Nine1 | Nine2 | Nine3 | Nine4 | Nine5 | Nine6
+          | Nine7 | Nine8
 
 
-modeRE :: ModeIdx -> TriState -> TriState -> TriState -> TriState
-                  -> TriState -> TriState -> TriState -> TriState
-                  -> TriState -> TriState -> TriState -> TriState -> ByteString
-modeRE    Idx0    On  _   _   _    _   _   _   _    _   _   _   _   =  "r"
-modeRE    Idx0    Off _   _   _    _   _   _   _    _   _   _   _   =  "-"
-modeRE    Idx0    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
-modeRE    Idx1    _   On  _   _    _   _   _   _    _   _   _   _   =  "w"
-modeRE    Idx1    _   Off _   _    _   _   _   _    _   _   _   _   =  "-"
-modeRE    Idx1    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
-modeRE    Idx2    _   _   On  On   _   _   _   _    _   _   _   _   =  "s"
-modeRE    Idx2    _   _   On  Off  _   _   _   _    _   _   _   _   =  "x"
-modeRE    Idx2    _   _   On  _    _   _   _   _    _   _   _   _   =  "[sx]"
-modeRE    Idx2    _   _   Off On   _   _   _   _    _   _   _   _   =  "S"
-modeRE    Idx2    _   _   Off Off  _   _   _   _    _   _   _   _   =  "-"
-modeRE    Idx2    _   _   Off _    _   _   _   _    _   _   _   _   =  "[sS]"
-modeRE    Idx2    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
-modeRE    Idx3    _   _   _   _    On  _   _   _    _   _   _   _   =  "r"
-modeRE    Idx3    _   _   _   _    Off _   _   _    _   _   _   _   =  "-"
-modeRE    Idx3    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
-modeRE    Idx4    _   _   _   _    _   On  _   _    _   _   _   _   =  "w"
-modeRE    Idx4    _   _   _   _    _   Off _   _    _   _   _   _   =  "-"
-modeRE    Idx4    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
-modeRE    Idx5    _   _   _   _    _   _   On  On   _   _   _   _   =  "s"
-modeRE    Idx5    _   _   _   _    _   _   On  Off  _   _   _   _   =  "x"
-modeRE    Idx5    _   _   _   _    _   _   On  _    _   _   _   _   =  "[sx]"
-modeRE    Idx5    _   _   _   _    _   _   Off On   _   _   _   _   =  "S"
-modeRE    Idx5    _   _   _   _    _   _   Off Off  _   _   _   _   =  "-"
-modeRE    Idx5    _   _   _   _    _   _   Off _    _   _   _   _   =  "[sS]"
-modeRE    Idx5    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
-modeRE    Idx6    _   _   _   _    _   _   _   _    On  _   _   _   =  "r"
-modeRE    Idx6    _   _   _   _    _   _   _   _    Off _   _   _   =  "-"
-modeRE    Idx6    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
-modeRE    Idx7    _   _   _   _    _   _   _   _    _   On  _   _   =  "w"
-modeRE    Idx7    _   _   _   _    _   _   _   _    _   Off _   _   =  "-"
-modeRE    Idx7    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
-modeRE    Idx8    _   _   _   _    _   _   _   _    _   _   On  On  =  "t"
-modeRE    Idx8    _   _   _   _    _   _   _   _    _   _   On  Off =  "x"
-modeRE    Idx8    _   _   _   _    _   _   _   _    _   _   On  _   =  "[tx]"
-modeRE    Idx8    _   _   _   _    _   _   _   _    _   _   Off On  =  "T"
-modeRE    Idx8    _   _   _   _    _   _   _   _    _   _   Off Off =  "-"
-modeRE    Idx8    _   _   _   _    _   _   _   _    _   _   Off _   =  "[tT]"
-modeRE    Idx8    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
+modeRE :: Nine -> TriState -> TriState -> TriState -> TriState
+               -> TriState -> TriState -> TriState -> TriState
+               -> TriState -> TriState -> TriState -> TriState -> ByteString
+modeRE    Nine0    On  _   _   _    _   _   _   _    _   _   _   _   =  "r"
+modeRE    Nine0    Off _   _   _    _   _   _   _    _   _   _   _   =  "-"
+modeRE    Nine0    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
+modeRE    Nine1    _   On  _   _    _   _   _   _    _   _   _   _   =  "w"
+modeRE    Nine1    _   Off _   _    _   _   _   _    _   _   _   _   =  "-"
+modeRE    Nine1    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
+modeRE    Nine2    _   _   On  On   _   _   _   _    _   _   _   _   =  "s"
+modeRE    Nine2    _   _   On  Off  _   _   _   _    _   _   _   _   =  "x"
+modeRE    Nine2    _   _   On  _    _   _   _   _    _   _   _   _   =  "[sx]"
+modeRE    Nine2    _   _   Off On   _   _   _   _    _   _   _   _   =  "S"
+modeRE    Nine2    _   _   Off Off  _   _   _   _    _   _   _   _   =  "-"
+modeRE    Nine2    _   _   Off _    _   _   _   _    _   _   _   _   =  "[sS]"
+modeRE    Nine2    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
+modeRE    Nine3    _   _   _   _    On  _   _   _    _   _   _   _   =  "r"
+modeRE    Nine3    _   _   _   _    Off _   _   _    _   _   _   _   =  "-"
+modeRE    Nine3    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
+modeRE    Nine4    _   _   _   _    _   On  _   _    _   _   _   _   =  "w"
+modeRE    Nine4    _   _   _   _    _   Off _   _    _   _   _   _   =  "-"
+modeRE    Nine4    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
+modeRE    Nine5    _   _   _   _    _   _   On  On   _   _   _   _   =  "s"
+modeRE    Nine5    _   _   _   _    _   _   On  Off  _   _   _   _   =  "x"
+modeRE    Nine5    _   _   _   _    _   _   On  _    _   _   _   _   =  "[sx]"
+modeRE    Nine5    _   _   _   _    _   _   Off On   _   _   _   _   =  "S"
+modeRE    Nine5    _   _   _   _    _   _   Off Off  _   _   _   _   =  "-"
+modeRE    Nine5    _   _   _   _    _   _   Off _    _   _   _   _   =  "[sS]"
+modeRE    Nine5    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
+modeRE    Nine6    _   _   _   _    _   _   _   _    On  _   _   _   =  "r"
+modeRE    Nine6    _   _   _   _    _   _   _   _    Off _   _   _   =  "-"
+modeRE    Nine6    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
+modeRE    Nine7    _   _   _   _    _   _   _   _    _   On  _   _   =  "w"
+modeRE    Nine7    _   _   _   _    _   _   _   _    _   Off _   _   =  "-"
+modeRE    Nine7    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
+modeRE    Nine8    _   _   _   _    _   _   _   _    _   _   On  On  =  "t"
+modeRE    Nine8    _   _   _   _    _   _   _   _    _   _   On  Off =  "x"
+modeRE    Nine8    _   _   _   _    _   _   _   _    _   _   On  _   =  "[tx]"
+modeRE    Nine8    _   _   _   _    _   _   _   _    _   _   Off On  =  "T"
+modeRE    Nine8    _   _   _   _    _   _   _   _    _   _   Off Off =  "-"
+modeRE    Nine8    _   _   _   _    _   _   _   _    _   _   Off _   =  "[tT]"
+modeRE    Nine8    _   _   _   _    _   _   _   _    _   _   _   _   =  "."
+
 
 
