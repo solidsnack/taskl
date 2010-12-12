38b52cca389b3aeb4381b689b80b4a08c7606cd6
Jason Dusek <jason.dusek@gmail.com>
2010-12-11 22:40:15 -0800

    Minimal exports from the Bash backend.

---
+++ System/TaskL/Bash.hs
@@ -0,0 +1,11 @@
+
+module System.TaskL.Bash
+  ( System.TaskL.Bash.Codegen.stateArrays
+  , System.TaskL.Bash.Codegen.code
+  , System.TaskL.Bash.Program.Term(..)
+  , System.TaskL.Bash.Program.ARGV(..)
+  ) where
+
+import System.TaskL.Bash.Codegen
+import System.TaskL.Bash.Program
+
