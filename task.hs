c498f56692351beeb26634c60c08b29bf0598691
Jason Dusek <jason.dusek@gmail.com>
2010-12-12 16:25:57 -0800

    Move tests to new format.

--- task.hs
+++ task.hs
@@ -8,30 +8,34 @@
 
 import Data.Tree
 
+import Data.Monoid
+
 import System.TaskL.IdemShell
 import System.TaskL.Combination
 import System.TaskL.IndexForest
 import System.TaskL.Task
 import System.TaskL.Op
 import System.TaskL.Schedule
+import System.TaskL
 
 
 tasks0 =
-  [ Node (Command (LNs "/foo" "/bar") []) []
+  [ Node (Command (LNs "/foo" "/bar") mempty) []
   ]
 
 tasks1 =
-  [ Node (Command (LNs "/foo" "/bar") []) []
-  , Node (Command (TOUCH "/x/a") []) [Node (Command (MKDIR "/x") []) []]
-  , Node (Command (TOUCH "/x/b") []) [Node (Command (MKDIR "/x") []) []]
+  [ Node (Command (LNs "/foo" "/bar") mempty) []
+  , Node (Command (TOUCH "/x/a") mempty)
+         [Node (Command (MKDIR "/x") mempty) []]
+  , Node (Command (TOUCH "/x/b") mempty)
+         [Node (Command (MKDIR "/x") mempty) []]
   ]
 
 tasks2 =
-  [ Node (Command (TOUCH "/q/a") []) [Node (Command (MKDIR "/q")
-                                       [DIFFq "./a" "./b"]) []]
-  , Node (Command (TOUCH "/q/b") []) [ Node (Command (MKDIR "/q")
-                                        [DIFFq "./c" "./d"]) []
-                                     , Node (Command (MKDIR "/q/p") []) []
-                                     ]
+  [ Node (Command (TOUCH "/q/a") mempty) [Node (Command (MKDIR "/q")
+                                        (DIFFq "./a" "./b")) []]
+  , Node (Command (TOUCH "/q/b") mempty)
+      [ Node (Command (MKDIR "/q") (DIFFq "./c" "./d")) []
+      , Node (Command (MKDIR "/q/p") mempty) [] ]
   ]
 
