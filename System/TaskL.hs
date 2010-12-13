7b4426ad3117f76b9cd8ea6fa0fb17cb95d326d7
Jason Dusek <jason.dusek@gmail.com>
2010-12-12 16:24:15 -0800

    Export more components of Task\L.

--- System/TaskL.hs
+++ System/TaskL.hs
@@ -30,6 +30,9 @@
 
 module System.TaskL where
 
+import System.TaskL.Combination
 import System.TaskL.Bash
-import System.TaskL.Schedule
+import System.TaskL.Schedule (schedule)
+import System.TaskL.IdemShell ( Command(..), Test(..), Ownership(..), Mode(..)
+                              , TriState(..), NodeType(..) )
 
