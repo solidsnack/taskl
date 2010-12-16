9f71d840be6be7f45f5153eba0b9eb38e3c646a1
Jason Dusek <jason.dusek@gmail.com>
2010-12-16 01:07:58 -0800

    Compiles; now can create chains of ops.

--- System/TaskL/Bash/PrettyPrinter.hs
+++ System/TaskL/Bash/PrettyPrinter.hs
@@ -1,4 +1,8 @@
 {-# LANGUAGE OverloadedStrings
+           , StandaloneDeriving
+           , RecordWildCards
+           , NamedFieldPuns
+           , NoMonomorphismRestriction
   #-}
 
 {-| Pretty printer for Bash. Not very pretty right now.
@@ -6,33 +10,89 @@
 
 module System.TaskL.Bash.PrettyPrinter where
 
-import Data.Binary.Builder
-import Data.ByteString
+import qualified Data.List as List
+import Data.Monoid
+import Prelude hiding (concat, length, replicate)
+import Data.Binary.Builder hiding (append)
+import Data.ByteString.Char8
+import Data.Word
 
 import System.TaskL.Bash.Program
 
 
-pp (i, l) term               =  case term of
-  SimpleCommand cmd vals    ->  mempty
-  Empty                     ->  b (pad `append` ":")
-  Bang t                    ->  b (pad `append` "! ") `mappend` pp (i, l+2) term
-  And t t'                  ->  pp i t `mappend` " &&\n" `mappend` pp i t'
-  Or t t' ->
-  Pipe t t' ->
-  Sequence t t' ->
-  Background t t' ->
-  Group t ->
-  Subshell t ->
-  IfThen t t' ->
-  IfThenElse t t' t'' ->
-  ForDoDone var vals t ->
-  VarAssign var val ->
-  DictDecl var pairs ->
-  DictUpdate var key val ->
-  DictAssign var pairs ->
+ops                         ::  Term -> [PPOp]
+ops term                     =  case term of
+  SimpleCommand cmd vals    ->  hang cmd ++ undefined ++ outdent
+  Empty                     ->  word ": 'Do nothing.'"
+  Bang t                    ->  hang "!" ++ ops t ++ outdent
+  And t t'                  ->  ops t ++ word "&&" ++ nl ++ ops t'
+  Or t t'                   ->  ops t ++ word "||" ++ nl ++ ops t'
+  Pipe t t'                 ->  ops t ++ word "|"  ++ nl ++ ops t'
+  Sequence t t'             ->  ops t              ++ nl ++ ops t'
+  Background t t'           ->  ops t ++ word "&"  ++ nl ++ ops t'
+  Group t                   ->  hang "{"  ++ ops t ++ word ";}" ++ outdent
+  Subshell t                ->  hang "("  ++ ops t ++ word ")"  ++ outdent
+  IfThen t t'               ->  hang "if" ++ ops t ++ outdent ++ nl
+                            ++  inword "then"      ++ ops t'  ++ outword "fi"
+  IfThenElse t t' t''       ->  hang "if" ++ ops t ++ outdent ++ nl
+                            ++  inword "then"      ++ ops t'  ++ outdent
+                            ++  inword "else"      ++ ops t'' ++ outword "fi"
+  ForDoDone var vals t      ->  hang (concat ["for ", var, " in"])
+                            ++  undefined
+                            ++  outdent ++ nl
+                            ++  inword "do" ++ ops t ++ outword "done"
+  VarAssign var val         ->  wordcat [var, "=", val]
+  DictDecl var pairs        ->  wordcat ["declare -A ", var, "=("] ++ nl
+                            ++  undefined
+                            ++  nl ++ word ")"
+  DictUpdate var key val    ->  wordcat [var, "[", key, "]=", val]
+  DictAssign var pairs      ->  wordcat [var, "=("] ++ nl
+                            ++  undefined
+                            ++  nl ++ word ")"
  where
-  b                          =  fromByteString
-  pad                        =  replicate i ' '
+  nl                         =  [Newline]
+  hang bytes                 =  [Word bytes, Indent (cast (length bytes) + 1)]
+  word bytes                 =  [Word bytes]
+  wordcat                    =  word . concat
+  backslash bytes            =  [Word "\\", Newline, Word bytes]
+  outdent                    =  [Outdent]
+  inword bytes               =  [Word bytes, Indent 2]
+  outword bytes              =  [Outdent, Word bytes]
+
+{-| State of pretty printing -- string being built, indent levels, whether
+    we've started a new line or not. 
+ -}
+data PPState                 =  PPState { indents :: [Word]
+                                        , flag :: Bool
+                                        , string :: Builder }
+
+{-| Operations we can perform while pretty printing:
+
+ *  Add n spaces to the indentation.
+
+ *  Strip off a level of indentation.
+
+ *  Move to the next line.
+
+ *  Append a shell word to the current line.
+
+ -}
+data PPOp = Indent Word | Outdent | Word ByteString | Newline
+
+
+{-| Apply an operation to a state. 
+ -}
+op                          ::  PPState -> PPOp -> PPState
+op state@PPState{..} x       =  case x of
+  Indent w                  ->  state {indents = w:indents}
+  Outdent                   ->  state {indents = ht indents}
+  Newline                   ->  state {flag = True}
+  Word b                    ->  state {string = string `mappend` pad b}
+ where
+  pad b = fromByteString $ if flag then replicate dent ' ' `append` b
+                                   else ' ' `cons` b
+  dent                       =  cast (sum indents)
+  ht                         =  List.head . List.tails
 
 {- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
@@ -52,4 +112,6 @@ pp (i, l) term               =  case term of
     DONE
 
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -}
+
+cast                         =  fromIntegral
 
