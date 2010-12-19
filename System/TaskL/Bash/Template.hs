0f98c3657d0266aff195fcf8ba900be0cf4dcffd
Jason Dusek <jason.dusek@gmail.com>
2010-12-18 17:32:13 -0800

    Correctly splits out generated code.

--- System/TaskL/Bash/Template.hs
+++ System/TaskL/Bash/Template.hs
@@ -2,7 +2,10 @@
            , OverloadedStrings
   #-}
 
-module System.TaskL.Bash.Template where
+module System.TaskL.Bash.Template ( template
+                                  , preamble
+                                  , postamble
+                                  , splitOnGeneratedCode ) where
 
 import Data.ByteString.Char8
 
@@ -11,4 +14,23 @@ import System.TaskL.Macros
 
 template                    ::  ByteString
 template                     =  $(pullFile "./example/example.bash")
+
+
+preamble                    ::  ByteString
+preamble                     =  fst templateSplitted
+
+postamble                   ::  ByteString
+postamble                    =  snd templateSplitted
+
+
+splitOnGeneratedCode        ::  ByteString -> (ByteString, ByteString)
+splitOnGeneratedCode bytes   =  (a `append` generatedCodeDelimiter, z)
+ where
+  (a, crud)                  =  breakSubstring generatedCodeDelimiter bytes
+  (_, z)                     =  breakSubstring (bar `append` "\n# Go.") crud
+  generatedCodeDelimiter     =  append bar "\n# Generated code."
+  bar = "################################################################"
+
+
+templateSplitted             =  splitOnGeneratedCode template
 
