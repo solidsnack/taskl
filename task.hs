a8f5af560aa6ed2755e3c0cec42ebd82254cbbbb
Jason Dusek <jason.dusek@gmail.com>
2010-12-26 14:21:11 -0800

    More sophisticated example.

--- task.hs
+++ task.hs
@@ -4,15 +4,19 @@
 
 
 import System.IO
+import qualified Data.Set as Set
 import Data.Tree
 import Data.Monoid
+import Data.Either
 import qualified Data.Binary.Builder
 import qualified Data.ByteString.Lazy
 import Data.ByteString.Lazy.Char8
 
 import Data.Digest.Pure.SHA
 
+import Language.TaskL.EncDec
 import Language.TaskL.IdemShell
+import Language.TaskL.IdemShell.PasswdDB
 import Language.TaskL.Combination
 import Language.TaskL.IndexForest
 import Language.TaskL.Task
@@ -42,18 +46,28 @@ tasks2 =
       , Node (Command (MKDIR "/q/p") mempty) [] ]
   ]
 
-tasks3 =
-  [ Node (Command (TOUCH "/q/a") mempty)
-      [ Node (Command (MKDIR "/q") (DIFFq "./a" "./b")) [] ]
-  , Node (Command (TOUCH "/q/b") mempty)
-      [ Node (Command (MKDIR "/q") (DIFFq "./c" "./d")) []
-      , Node (Command (MKDIR "/q/p") mempty) [] ]
-  ]
-
+tasks3 = do
+  root <- dec "root"
+  admin <- dec "admin"
+  admin' <- dec "admin"
+  return
+    [ Node (Command (GPASSWDm admin (Set.fromList ["solidsnack"]) Set.empty)
+                    mempty)
+           []
+    , Node (Command (CP "./etc/sudoers" "/etc/sudoers") mempty) []
+    , Node (Command (CHMOD "/etc/sudoers"
+                           (Mode On On Off Off On Off Off Off Off Off Off Off))
+                    mempty)
+           [Node (Command (CP "./etc/sudoers" "/etc/sudoers") mempty) []]
+    , Node (Command (CHOWN "/etc/sudoers" (Both root admin')) mempty)
+           [Node (Command (CP "./etc/sudoers" "/etc/sudoers") mempty) []]
+    ]
 
 code2                        =  TaskL.script tasks2
 
-main                         =  case code2 of
+code3                        =  TaskL.script (either (const []) id tasks3)
+
+main                         =  case code3 of
   Right (bytes, warnings)   ->  Data.ByteString.Lazy.hPut stdout bytes
   Left errors               ->  Data.ByteString.Lazy.hPut stderr "Error!"
 
