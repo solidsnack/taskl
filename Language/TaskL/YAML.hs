309c01fecef7c1737868226c198deb34c0dcfd53
Jason Dusek <jason.dusek@gmail.com>
2010-12-26 14:27:05 -0800

    Still working on this.

--- Language/TaskL/YAML.hs
+++ Language/TaskL/YAML.hs
@@ -1,13 +1,68 @@
 
 module Language.TaskL.YAML where
 
-import Data.ByteString
+import Data.Tree
+import qualified Data.Set as Set
+import Data.Set (Set)
+import Data.ByteString.Char8 hiding (map)
 
-import Data.Yaml.YamlLight
+import qualified Data.Object as YO
+import qualified Data.Object.Yaml as YO
+
+import Language.TaskL.EncDec
+import Language.TaskL.Task
+import Language.TaskL.IdemShell
+import Language.TaskL.Codegen.IdemShell (modeSymbolic)
 
 
-unYAML                      ::  YamlLight -> [Tree Task]
+decodeTasks                 ::  YO.YamlObject -> [Tree Task]
+decodeTasks                  =  undefined
 
-toYAML                      ::  [Tree Task] -> YamlLight
+encodeTasks                 ::  [Tree Task] -> YO.YamlObject
+encodeTasks                  =  YO.Sequence . map encodeTaskNode
 
+encodeTaskNode (Node task tasks) =
+  YO.Mapping [((YO.toYamlScalar . yamlName) task, undefined)]
+
+encodeTask (Command cmd test)  = YO.Sequence [argSequence cmd]
+ where
+  argSequence                =  YO.Sequence . map YO.toYamlScalar . args
+encodeTask (Package name test) = YO.Mapping [(undefined,undefined)]
+
+
+optionalTest task            =  Nothing
+
+
+yamlName                    ::  Task -> ByteString
+yamlName (Package name _)    =  name `snoc` '/'
+yamlName (Command cmd _)     =  case cmd of
+  CHOWN _ _                 ->  "CHOWN"
+  CHMOD _ _                 ->  "CHMOD"
+  RM _                      ->  "RM"
+  CP _ _                    ->  "CP"
+  LNs _ _                   ->  "LNs"
+  TOUCH _                   ->  "TOUCH"
+  MKDIR _                   ->  "MKDIR"
+  USERADD _ _               ->  "USERADD"
+  USERDEL _                 ->  "USERDEL"
+  GROUPADD _ _              ->  "GROUPADD"
+  GROUPDEL _                ->  "GROUPDEL"
+  GPASSWDm _ _ _            ->  "GPASSWDm"
+
+args                        ::  Command -> [ByteString]
+args cmd                     =  case cmd of
+  CHOWN p o                 ->  [enc p, chownStyle o]
+  CHMOD p m                 ->  [enc p, modeSymbolic m]
+  RM p                      ->  [enc p]
+  CP p' p                   ->  [enc p', enc p]
+  LNs p' p                  ->  [enc p', enc p]
+  TOUCH p                   ->  [enc p]
+  MKDIR p                   ->  [enc p]
+  USERADD nick _            ->  [enc nick]
+  USERDEL nick              ->  [enc nick]
+  GROUPADD nick _           ->  [enc nick]
+  GROUPDEL nick             ->  [enc nick]
+  GPASSWDm gNick inU outU   ->  enc gNick : f '+' inU ++ f '-' outU
+   where
+    f char                   =  map (cons char . enc) . Set.toList
 
