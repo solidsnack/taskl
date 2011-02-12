4c78bb80e41440f49dcaffcb656d30687a051436
Jason Dusek <jason.dusek@gmail.com>
2011-02-12 02:19:40 -0800

    Monoids.

--- Language/TaskL/Syntax.hs
+++ Language/TaskL/Syntax.hs
@@ -1,6 +1,9 @@
-
+{-# LANGUAGE StandaloneDeriving
+           , EmptyDataDecls
+  #-}
 module Language.TaskL.Syntax where
 
+import Data.Monoid
 import Data.ByteString (ByteString)
 import Data.Map (Map)
 import Data.Set (Set)
@@ -19,4 +22,19 @@ newtype StringL              =  StringL [StringLToken]
 data StringLToken            =  Literal ByteString
                              |  Substitution ByteString
 
+newtype Task                 =  Task ByteString
+deriving instance Monoid Task
 
+newtype Test                 =  Test ByteString
+deriving instance Monoid Test
+
+instance Monoid Module where
+  mempty                     =  Module mempty
+  mappend (Module x) (Module y) = Module (mappend x y)
+instance Monoid TaskBody where
+  mempty                     =  TaskBody Nothing Nothing mempty
+  mappend (TaskBody x y z) (TaskBody x' y' z') =
+    TaskBody (mappend x x') (mappend y y') (mappend z z')
+deriving instance Ord Name
+deriving instance Eq Name
+deriving instance Monoid Name
