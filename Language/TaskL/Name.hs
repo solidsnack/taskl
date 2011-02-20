3cc02d659b68f72788779be620ba4e6730602e09
Jason Dusek <jason.dusek@gmail.com>
2011-02-20 04:25:29 +0000

    Ugly name parsing code works.

--- Language/TaskL/Name.hs
+++ Language/TaskL/Name.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE OverloadedStrings
+           , StandaloneDeriving
+  #-}
+
 
 module Language.TaskL.Name where
 
 import Data.Monoid
-import Data.ByteString.Char8
+import Data.ByteString.Char8 hiding (reverse, map)
 import Data.Map (Map)
 import Data.Set (Set)
 import Data.String
@@ -10,17 +14,45 @@ import Data.String
 import Text.Regex.XMLSchema.String
 
 
-newtype Name                 =  Name ByteString
+newtype Name                 =  Name { bytes :: ByteString }
+deriving instance Eq Name
+deriving instance Ord Name
+deriving instance Show Name
 
 
-names                       ::  ByteString -> Maybe (Name, [Name])
-names bytes                  =  undefined
+names :: ByteString -> Either (ByteString, ByteString) (Name, [Name])
+names b                      =  case popName (unpack b) of
+  Right (name, s)           ->  names' (name, []) s
+  Left err                  ->  Left err
  where
-  s                          =  unpack bytes
+  names' (name, names) ""    =  Right (name, reverse names)
+  names' (name, names) s     =  case popName s of
+    Right (name', s')       ->  names' (name, name':names) s'
+    Left (before, after)    ->  Left (append repacked (cons ':' before), after)
+   where
+    repacked = (intercalate ":" . map bytes) (name : reverse names)
 
-popName                      =  splitSubex namesRE
+popName :: String -> Either (ByteString, ByteString) (Name, String)
+popName s = maybe (Left (pack s, "")) rewrite (splitSubexRE namesRE s)
+ where
+  repack a b                 =  append (pack a) (':' `cons` pack b)
+  rewrite res                =  case res of
+    ([("name",""),("tail","")],"")  ->  Left  (pack s, "")
+    ([("name",n),("tail",t)]  ,"")  ->  Right (Name (pack n), t)
+    ([("name",n),("tail",t)],trash) ->  Left  (repack n t, pack trash)
+    ([("name","")]          ,  "")  ->  Left  (pack s, "")
+    ([("name",n)]           ,  "")  ->  Right (Name (pack n), "")
+    ([("name",n)]           ,trash) ->  Left  (pack n, pack trash)
+    (_                      ,   _)  ->  Left  (pack s, "")
 
-namesRE = "({name}" ++ nameRE ++ ")" ++ "({tail}(:" ++ nameRE ++ ")*)"
-nameRE                       =  "[a-z_][a-zA-Z0-9_]*"
+namesRE                      =  mkBr "name" nameRE
+                             /  mkOpt $ colon
+                             /  mkBr "tail" $ nameRE
+                             /  mkStar (colon / nameRE)
+ where
+  infixr 0 /
+  (/)                        =  mkSeq
+  colon                      =  mkSym1 ':'
 
+nameRE                       =  parseRegex "[a-z_][a-zA-Z0-9_]*"
 
