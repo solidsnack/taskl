b194bec5583a2713a418d78f430b16763a9912b2
Jason Dusek <jason.dusek@gmail.com>
2012-11-09 13:37:30 +0000

    wip

--- TaskL.hs
+++ TaskL.hs
@@ -7,6 +7,12 @@ import qualified Data.ByteString.Char8 as ByteString
 import           Data.Either
 import           Data.Monoid
 import           Data.String
+import           Data.Set (Set)
+import qualified Data.Set as Set
+import           Data.Map (Map)
+import qualified Data.Map as Map
+import           Data.Tree (Tree(..), Forest)
+import qualified Data.Tree as Tree
 
 import           Data.Graph.Wrapper
 
@@ -17,7 +23,7 @@ data Task = Task Command [Argument]
 data Command = ShHTTP ByteString | Path ByteString
  deriving (Eq, Ord, Show)
 
-data Argument = Literal ByteString | Expandable ByteString
+data Argument = Literal ByteString
  deriving (Eq, Ord, Show)
 instance IsString Argument where
   fromString = Literal . ByteString.pack
@@ -34,4 +40,25 @@ schedule g | a == []   = Right b
  where (a, b) = partitionEithers (scc2either <$> stronglyConnectedComponents g)
        scc2either (CyclicSCC ts) = Left ts
        scc2either (AcyclicSCC t) = Right t
+
+
+-- | When input documents are read, they present tasks and dependencies as
+--   trees. These trees are collapsed to an adjacency list representation, for
+--   use by a graph library.
+adjacencies :: Tree Task -> [(Task, [Task])]
+adjacencies (Node t [ ]) = (t, []) : []
+adjacencies (Node t sub) = (t, Tree.rootLabel <$> sub)
+                         : concatMap adjacencies sub
+
+-- | A task could be present multiple times in a document or collection of
+--   documents. Dependencies are always merged across multiple definitions of
+--   a task, by taking the set union of them.
+merge :: [(Task, [Task])] -> Map Task (Set Task)
+merge  = (Set.fromList <$>) . Map.fromListWith (++)
+
+-- | Once input documents are parsed, they present a forest of tasks. These
+--   tasks are merged to form the final, compilable graph.
+graph :: Forest Task -> Graph Task Task
+graph  = fromListSimple . Map.toAscList . (Set.toAscList <$>)
+       . merge . concatMap adjacencies
 
