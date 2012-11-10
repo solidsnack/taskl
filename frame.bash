68d563cd8bf592adf0d1d71649713e80ef243abc
Jason Dusek <jason.dusek@gmail.com>
2012-11-10 18:25:37 +0000

    Improved argument handling.

--- frame.bash
+++ frame.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
 
+function tasks {
+  : # Replace with worker body.
+}
+
 function curl_sh {
   local url="$1" ; shift
   curl_ "$url" | sh -s -- "$@"
@@ -14,5 +18,8 @@ function msg { out "$*" 1>&2 ;}
 function err { local x=$? ; msg "$*" ; return $(( $x == 0 ? 1 : $x )) ;}
 function out { printf '%s\n' "$*" ;}
 
-"$@"
+if declare -F | cut -d' ' -f3 | fgrep -qx -- "${1:-}"
+then "$@"       # Recognized function so call with arguments.
+else tasks "$@" # Unrecognized so use default function.
+fi
 
