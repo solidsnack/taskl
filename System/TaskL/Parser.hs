b0a257c2d9aec84cf781046eef6091e3e8c37163
Jason Dusek <jason.dusek@gmail.com>
2013-01-09 19:31:01 +0000

    Work in progress.

--- System/TaskL/Parser.hs
+++ System/TaskL/Parser.hs
@@ -38,23 +38,31 @@ import qualified Data.Vector as Vector
 import qualified Data.Yaml as Yaml
 
 
-data Declaration = Declaration ByteString Definition
- deriving (Eq, Ord, Show)
+data Declaration = Declaration ByteString Definition deriving (Eq, Ord, Show)
 
-data Definition = Definition [Var] [Forest Call]
- deriving (Eq, Ord, Show)
+data Definition = Definition [Var] [Tree Call] deriving (Eq, Ord, Show)
 
-data Call = Call (Type, ByteString) [TemplateString]
- deriving (Eq, Ord, Show)
+data Call = Call (Type, ByteString) [TemplateString] deriving (Eq, Ord, Show)
 
 deriving instance Ord t => Ord (Tree t)
 
 data Type = Task | Path | HTTP deriving (Eq, Ord, Show)
 
-newtype Var = Var ByteString deriving (Eq, Ord, Show, IsString)
+data Var = Var ByteString (Maybe ByteString)
+ deriving (Eq, Ord, Show)
 
 newtype TemplateString = TemplateString [Either Var ByteString]
  deriving (Eq, Ord, Show)
+
+
+declarations :: Aeson.Value -> Aeson.Parser [Declaration]
+declarations (Aeson.Object o) = undefined
+declarations _                = mzero
+
+instance Aeson.FromJSON Definition where
+  parseJSON json@(Aeson.Object o) = Definition [] <$> parseForest json
+   where vars = HashMap.lookup "_" o
+  parseJSON _ = mzero
 
 instance Aeson.FromJSON Call where
   parseJSON (Aeson.Object o) = maybe mzero call (listToMaybe cmds)
@@ -66,32 +74,36 @@ instance Aeson.FromJSON Call where
          call _                      = mzero
   parseJSON _ = mzero
 
-instance Aeson.FromJSON (Tree Call) where
-  parseJSON json@(Aeson.Object o)
-    | Just _ <- forest >> branch     = mzero -- Fail if both are present.
-    | Just (Aeson.Array v) <- forest = node <*> parseV v
-    | Just (Aeson.Array v) <- branch = node <*> (listToBiasTree <$> parseV v)
-    | otherwise                      = mzero
-   where branch  = HashMap.lookup ";" o
-         forest  = HashMap.lookup "." o
-         node    = Node <$> parseJSON json
-         parseV  = mapM parseJSON . toList
-         Node call forest << other = Node call (other:forest) 
-         listToBiasTree (a:b:rest) = listToBiasTree ((b<<a):rest)
-         listToBiasTree list       = list
-  parseJSON _ = mzero
-
 instance Aeson.FromJSON TemplateString where
   parseJSON json = case json of
     Aeson.String t -> return $ TemplateString [bytes t]
+    Aeson.Object _ -> TemplateString . (:[]) <$> pick json
     Aeson.Array v  -> TemplateString <$> mapM pick (toList v) 
     _              -> mzero
    where bytes = Right . Text.encodeUtf8
          pick (Aeson.String t) = return $ bytes t
          pick (Aeson.Object o) = case parseableKeys label o of
-                                   (t,_):_ -> return . Left $ Var t
+                                   [(t,_)] -> return . Left $ Var t Nothing
                                    _       -> mzero
          pick _                = mzero
+
+parseTree :: Aeson.Value -> Aeson.Parser (Tree Call)
+parseTree json@(Aeson.Object _) = Node <$> parseJSON json <*> parseForest json
+parseTree _                     = mzero
+
+parseForest :: Aeson.Value -> Aeson.Parser [Tree Call]
+parseForest (Aeson.Object o)
+  | Just _ <- forest >> branch     = mzero -- Fail if both are present.
+  | Just (Aeson.Array v) <- forest = parseTrees v
+  | Just (Aeson.Array v) <- branch = biasForest <$> parseTrees v
+  | otherwise                      = mzero
+ where branch = HashMap.lookup ";" o
+       forest = HashMap.lookup "." o
+       parseTrees = mapM parseTree . toList
+       biasForest (n : Node l f : rest) = biasForest (Node l (n:f) : rest)
+       biasForest list                  = list
+parseForest _ = mzero
+
 
 -- | Scan a JSON object for keys that match a certain parser.
 parseableKeys :: Attoparsec.Parser t -> Aeson.Object -> [(t, Aeson.Value)]
