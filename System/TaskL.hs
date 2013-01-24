6a3b4ac522ba74142ff3a2a3757f27a0a98a1275
Jason Dusek <jason.dusek@gmail.com>
2013-01-24 18:55:31 +0000

    Compiler phases; simplified string types.

--- System/TaskL.hs
+++ System/TaskL.hs
@@ -1,203 +1,75 @@
-{-# LANGUAGE OverloadedStrings
-           , TupleSections
-           , TemplateHaskell
-           , ScopedTypeVariables
-           , GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving
+           , TypeOperators
+           , TypeFamilies
+           , UndecidableInstances #-}
 module System.TaskL where
 
-import           Control.Applicative
-import           Control.Arrow
-import           Control.Monad
 import           Data.ByteString.Char8 (ByteString)
-import qualified Data.ByteString.Char8 as ByteString
-import           Data.Either
-import           Data.Foldable (toList)
-import           Data.Monoid
-import           Data.Set (Set)
-import qualified Data.Set as Set
 import           Data.Map (Map)
-import qualified Data.Map as Map
 import           Data.Tree (Tree(..), Forest)
-import qualified Data.Tree as Tree
 
-import qualified Data.Attoparsec.ByteString.Char8 as Attoparsec
-import           Data.FileEmbed
-import           Data.Graph.Wrapper (Graph)
-import qualified Data.Graph.Wrapper as Graph
-import           Data.Yaml
 import qualified Language.Bash as Bash
 
-import           System.TaskL.JSONTree
+import           System.TaskL.Strings (Label(), Name())
 
 
-data Task = Task Call [ByteString] deriving (Eq, Ord, Show)
+class Task phase where
+  type Arg phase
+  type Cmd phase
+  type Cmd phase = Arg phase
+  type Ref phase
+  type Ref phase = Name
 
-data Call = Cmd Cmd | Abstract ByteString deriving (Eq, Ord, Show)
+-- | Type of inputs that are allowed to contain template variables.
+data Template = Template [Label] (Knot Template) deriving (Eq, Ord, Show)
+instance Task Template where
+  type Arg Template = [Either Label ByteString]
+  type Ref Template = [Either Label Name]
 
-data Cmd = ShHTTP ByteString | Path ByteString deriving (Eq, Ord, Show)
+-- | Type of inputs with remote references, requiring download.
+newtype Partial = Partial (Knot Partial) deriving (Eq, Ord, Show)
+instance Task Partial where
+  type Arg Partial = ByteString
+  type Cmd Partial = Either ByteString ByteString
 
+-- | Type of inputs that are ready for serialization as Bash.
+newtype Static = Static (Knot Static) deriving (Eq, Ord, Show)
+instance Task Static where
+  type Arg Static = ByteString
 
- ----------------- Parsing (input in raw and semi-raw forms) ------------------
+data Use t = Use { task :: Ref t,
+                   args :: [Arg t],
+                   with :: Forest (Use t) }
+deriving instance (Eq (Arg t), Eq (Ref t)) => Eq (Use t)
+deriving instance (Ord (Arg t), Ord (Ref t)) => Ord (Use t)
+deriving instance (Show (Arg t), Show (Ref t)) => Show (Use t)
 
-tasks :: Tree [ByteString] -> Either String (Tree Task)
-tasks (Node argv subs) = Node <$> task argv <*> mapM tasks subs
+data Knot t = Knot { code :: Code t,
+                     uses :: Forest (Use t),
+                     asks :: Forest (Use t) }
+deriving instance (Eq (Code t), Eq (Use t)) => Eq (Knot t)
+deriving instance (Ord (Code t), Ord (Use t)) => Ord (Knot t)
+deriving instance (Show (Code t), Show (Use t)) => Show (Knot t)
 
-task :: [ByteString] -> Either String Task
-task [   ] = Left "Empty argument vector."
-task (h:t) = Task <$> Attoparsec.parseOnly call h <*> Right t
+data Code t = Commands [(Cmd t, [Arg t])]
+deriving instance (Eq (Arg t), Eq (Cmd t)) => Eq (Code t)
+deriving instance (Ord (Arg t), Ord (Cmd t)) => Ord (Code t)
+deriving instance (Show (Arg t), Show (Cmd t)) => Show (Code t)
 
-call :: Attoparsec.Parser Call
-call  = (Abstract <$> name) <|> (Cmd <$> cmd)
-
-cmd :: Attoparsec.Parser Cmd
-cmd  = (ShHTTP <$> url) <|> (Path <$> Attoparsec.takeByteString)
-
-url :: Attoparsec.Parser ByteString
-url  = (<>) <$> (Attoparsec.string "http://" <|> Attoparsec.string "https://")
-            <*> Attoparsec.takeByteString
-
-name :: Attoparsec.Parser ByteString
-name  = (<>) <$> Attoparsec.string "//"
-             <*> (ByteString.intercalate "." <$> labels)
-             <*  Attoparsec.endOfInput
- where labels = Attoparsec.sepBy1 label (Attoparsec.char '.')
-
-label :: Attoparsec.Parser ByteString
-label  = label' <|> ByteString.singleton <$> ld
- where ldu = Attoparsec.takeWhile1 (Attoparsec.inClass "a-zA-Z0-9_")
-       ld  = Attoparsec.satisfy (Attoparsec.inClass "a-zA-Z0-9")
-       label' = do b <- ByteString.cons <$> ld <*> ldu
-                   if ByteString.last b == '_' then mzero else return b
-
-
- --------- Graph utilities (many representations of graphs are used) ----------
-
--- | Provide a traversal of the graph that visits each node once if there are
---   no cycles or return the cycles.
-schedule :: (Eq t) => Graph t t -> Either [[t]] [t]
-schedule g = if cycles == [] then Right traversal else Left cycles
- where (cycles, traversal) = partitionEithers (scc2either <$> sccs)
-       sccs = Graph.stronglyConnectedComponents g
-       scc2either (Graph.CyclicSCC ts) = Left ts
-       scc2either (Graph.AcyclicSCC t) = Right t
-
--- | Convert a forest into an adjacency list, backed by a 'Map'.
-forestMap :: (Ord t) => Forest t -> Map t (Set t)
-forestMap  = asMap . concatMap adjacencies
-
--- | Transform a tree to an adjacency list representation of its edges.
---   Duplicate edges are retained.
-adjacencies :: Tree t -> [(t, [t])]
-adjacencies (Node t [ ]) = (t, []) : []
-adjacencies (Node t sub) = (t, Tree.rootLabel <$> sub)
-                         : concatMap adjacencies sub
-
--- | Merge an adjacency list represented as tuples to a representation backed
---   by maps of sets, eliminating duplicate edges.
-asMap :: (Ord t) => [(t, [t])] -> Map t (Set t)
-asMap  = (Set.fromList <$>) . Map.fromListWith (++)
-
--- | Convert from a 'Map' representation of adjacency lists to a list backed
---   representation.
-unMap :: (Ord t) => Map t (Set t) -> [(t, [t])]
-unMap  = Map.toAscList . (Set.toAscList <$>)
-
--- | Merge adjacency lists and produce a graph.
-graph :: (Ord t) => Map t (Set t) -> Graph t t
-graph  = Graph.fromListSimple . unMap
-
--- | Clip an adjacency list to include only those entries reachable from the
---   given keys. If any keys aren't in the map, then 'Nothing' is returned.
-cull :: (Ord t) => [t] -> Map t (Set t) -> Maybe (Map t (Set t))
-cull list m = reachable <$ guard (all (flip Map.member m) list)
- where keys = Set.fromList (concatMap (Graph.reachableVertices (graph m)) list)
-       reachable = Map.fromList [ (k,v) | (k,v) <- Map.toList m
-                                        , Set.member k keys ]
-
--- | Each of the elements in the list is used to filter the forest, marking
---   subtrees for inclusion if they are rooted in one of these elements. The
---   elements of the subtrees so marked are then checked against the top
---   level, and their subtrees included if found. This goes on until there is
---   nothing left to add.
-trim :: (Ord t) => [t] -> Forest t -> Forest t
-trim ts forest = [ Node k v | (k, v) <- Map.toList (Map.filterWithKey f map) ]
- where f k _ = Set.member k (reachable (Set.fromList ts) map)
-       map   = roots forest
-
-roots :: (Ord t) => Forest t -> Map t (Forest t)
-roots f = Map.fromListWith (++) [ (t, sub) | Node t sub <- f ]
-
-reachable :: (Ord t) => Set t -> Map t (Forest t) -> Set t
-reachable ts m = recurse ts
- where keys t  = filter (`Map.member` m) . concatMap toList <$> Map.lookup t m
-       recurse set = if set' == set then set else recurse set'
-        where set_ = Set.fromList (concatMap (maybe [] id . keys) $ toList set)
-              set' = set_ <> set
+data Module t = Module { from :: ByteString, defs :: Map Name t } 
 
 
- ----------------------------- Shell generation -------------------------------
+-- | Compiler passes.
+type a :~ b = a -> IO b 
 
--- | Embed commands in the template script.
-script :: [(ByteString, [ByteString])] -> ByteString
-script list = header <> Bash.bytes (functionTasks list) <> "\n" <> footer
+template :: Module Template -> Map Name ByteString :~ Module Partial
+template  = undefined
 
--- | Render a command to abstract Bash.
-compile :: (ByteString, [ByteString]) -> Bash.Statement ()
-compile (cmd, args) = Bash.SimpleCommand (Bash.literal cmd)
-                                         (Bash.literal <$> args)
+fetch :: Module Partial :~ Module Static
+fetch  = undefined
 
--- | Translate a task to a concrete shell command. Abstract tasks become a
---   message announcing their completion.
-command :: Task -> (ByteString, [ByteString])
-command (Task (Cmd (ShHTTP url)) args) = ("tailed", "curl_sh":url:args)
-command (Task (Cmd (Path path))  args) = ("tailed", path:args)
-command (Task (Abstract name)       _) = ("msg",    name:[])
+bash :: Module Static :~ Bash.Annotated ()
+bash  = undefined
 
--- | Generate Bash function, called @tasks@, with the commands in sequence.
-functionTasks :: [(ByteString, [ByteString])] -> Bash.Statement ()
-functionTasks  = Bash.Function "tasks" . anno . and . (compile <$>)
- where anno = Bash.Annotated ()
-       and cmds = case cmds of [   ] -> Bash.SimpleCommand "msg" ["No tasks."]
-                               [cmd] -> cmd
-                               cmd:t -> Bash.AndAnd (anno cmd) (anno (and t))
-
-frame, header, footer :: ByteString
-frame            = $(embedFile "frame.bash")
-(header, footer) = (ByteString.unlines *** ByteString.unlines)
-                 . second (drop 1 . dropWhile (/= "}"))
-                 . span (/= "function tasks {")
-                 $ ByteString.lines frame
-
-
- --------------------------- Serialization routines ---------------------------
-
--- | Load a YAML document (and maybe JSON document?) as a tree of 'ByteString'.
-trees :: ByteString -> Maybe (Forest ByteString)
-trees  = decode
-
--- | Load a document acceptable to 'trees' and try to parse the tasks.
-load :: ByteString -> ([String], Forest Task)
-load  = maybe (["Unparseable input."],[])
-              (partitionEithers . (tasks . tree2tree <$>))
-      . trees
-
--- | Present each abstract task with its abstract dependencies below it.
-dependencies :: Map Task (Set Task) -> Forest ByteString
-dependencies m =
-  [ node | node@(Node s _) <- walk Set.empty <$> Map.keys m, s /= "" ]
- where lm = Set.toAscList <$> m
-       walk :: Set Task -> Task -> Tree ByteString
-       walk set task = Node label (maybe [] id children)
-        where cycle = Set.member task set
-              label = case task of
-                Task (Abstract b) _ -> if cycle then "!!! "<>b else b
-                _                   -> ""
-              recurse  = stepOverEmpties . walk (Set.insert task set)
-              children = concatMap recurse <$> Map.lookup task lm
-       stepOverEmpties (Node s c) = if s == "" then c' else [Node s c']
-        where c' = concatMap stepOverEmpties c
-
-draw :: Forest ByteString -> ByteString
-draw  = ByteString.unlines . concatMap (draw' "")
- where draw' a (Node b c) = a <> b : concatMap (draw' ("  "<>a)) c
+deriving instance (Ord t) => Ord (Tree t)
 
---
+++ System/TaskL/Strings.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE OverloadedStrings
+           , FlexibleInstances
+           , MultiParamTypeClasses
+           , FlexibleContexts
+           , UndecidableInstances
+  #-}
+-- | Utilities for plain text and restricted syntaxes for names.
+module System.TaskL.Strings where
+
+import           Control.Applicative
+import           Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as ByteString
+import           Data.Monoid
+
+import qualified Data.Attoparsec.ByteString.Char8 as Attoparsec
+import           Data.Text (Text)
+import qualified Data.Text.Encoding as Text
+
+
+newtype Name  = Name [Label] deriving (Eq, Ord, Show)     -- ^ Dots and labels.
+newtype Label = Label ByteString deriving (Eq, Ord, Show)
+                                         -- ^ A string matching @[a-zA-Z0-9-]+@
+
+class Str t s where
+  unStr :: s -> Either String t
+  toStr :: t -> s
+instance Str Name ByteString where
+  unStr = Attoparsec.parseOnly name
+  toStr (Name labels) = "//" <> ByteString.intercalate "." (toStr <$> labels)
+instance Str Label ByteString where
+  unStr = Attoparsec.parseOnly label
+  toStr (Label b) = b
+instance Str t ByteString => Str t String where
+  unStr = unStr . ByteString.pack
+  toStr = ByteString.unpack . toStr
+instance Str t ByteString => Str t Text where
+  unStr = unStr . Text.encodeUtf8
+  toStr = Text.decodeUtf8 . toStr
+
+
+url :: Attoparsec.Parser ByteString
+url  = (<>) <$> (Attoparsec.string "http://" <|> Attoparsec.string "https://")
+            <*> Attoparsec.takeByteString
+
+name :: Attoparsec.Parser Name
+name  = Attoparsec.string "//" *> (Name <$> labels)
+ where labels = Attoparsec.sepBy1 label (Attoparsec.char '.')
+
+label :: Attoparsec.Parser Label
+label  = Label <$> Attoparsec.takeWhile1 (Attoparsec.inClass "a-zA-Z0-9-")
+
+utf8 :: Text -> ByteString
+utf8  = Text.encodeUtf8
+
